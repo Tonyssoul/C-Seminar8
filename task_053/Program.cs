842e4ec3f7e9135757432a7ee64169b481998047
Anton Kargapolov <igloooooook@gmail.com>
2023-03-18 10:34:39 +0500

    task 053

---
+++ task_053/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю строку массива.
+
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, cols];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j]= rand.Next(0,11);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i,j]+"\t");
+        }
+        System.Console.WriteLine();
+    }
+    
+}
+
+void Zamena(int[,] matrix)
+{
+    int temp;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        temp = matrix[0,i];
+        matrix[0,i] =  matrix[matrix.GetLength(0)-1,i];
+        matrix[matrix.GetLength(0)-1,i] = temp;
+    }
+}
+
+
+int rows = ReadInt("Введите колличество строк матрицы ");
+int cols = ReadInt("Введите колличество столбцов матрицы ");
+System.Console.WriteLine();
+var myMatrix = GenerateMatrix(rows, cols);
+PrintMatrix(myMatrix);
+System.Console.WriteLine();
+Zamena(myMatrix);
+PrintMatrix(myMatrix);
